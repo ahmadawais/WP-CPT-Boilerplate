3831b8ddc043cbab4054891eb82fab70538d9480
Ahmad Bilal <atbilal@gmail.com>
2017-10-03 21:18:08 +0500

    Added files

---
+++ README.md
@@ -0,0 +1,1 @@
+# WordPress Custom Post Type Boilerplate
---
+++ wp-cpt-plugin.php
@@ -0,0 +1,31 @@
+<?php
+/**
+ * 
+ * This file is read by WordPress to generate the plugin information in the plugin
+ * admin area. This file also includes all of the dependencies used by the plugin,
+ * registers the activation and deactivation functions, and defines a function
+ * that starts the plugin.
+ *
+ * @link              http://example.com
+ * @since             1.0.0
+ * @package           Wp_Cpt_Plugin
+ *
+ * @wordpress-plugin
+ * Plugin Name:       WP Custom Post Type Boilerplate
+ * Plugin URI:        http://example.com/plugin-name-uri/
+ * Description:       This is a custom post type plugin boilerplate
+ * Version:           1.0.0
+ * Author:            Your Name or Your Company
+ * Author URI:        http://example.com/
+ * License:           GPL-2.0+
+ * License URI:       http://www.gnu.org/licenses/gpl-2.0.txt
+ * Text Domain:       plugin-name
+ * Domain Path:       /languages
+ */
+
+// If the plugin file is called directly outside WordPress, abort.
+if ( ! defined( 'WPINC' ) ) {
+	die;
+}
+
+define( 'PLUGIN_NAME_VERSION', '1.0.0' );
