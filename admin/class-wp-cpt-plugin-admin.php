5644bd91f797518a48caff494d2faf06d54641d2
Ahmad Bilal <atbilal@gmail.com>
2017-10-07 21:52:03 +0500

    Process

--- admin/class-wp-cpt-plugin-admin.php
+++ admin/class-wp-cpt-plugin-admin.php
@@ -109,43 +109,44 @@ class Wp_Cpt_Plugin_Admin {
 		*/
 
 		public static function new_cpt () {
-			$cap_type = 'post';
-			$plural = 'Custom Post Types';
-			$single = 'Custom Post Type';
-			$cpt_name = 'new_cpt';
-			$opts['can_export'] = TRUE;
-			$opts['capability_type'] = $cap_type;
-			$opts['description'] = '';
-			$opts['exclude_from_search'] = FALSE;
-			$opts['has_archive'] = FALSE;
-			$opts['hierarchical'] = FALSE;
-			$opts['map_meta_cap'] = TRUE;
-			$opts['menu_icon'] = 'dashicons-businessman';
-			$opts['menu_position'] = 25;
-			$opts['public'] = TRUE;
-			$opts['publicly_querable'] = TRUE;
-			$opts['query_var'] = TRUE;
-			$opts['register_meta_box_cb'] = '';
-			$opts['rewrite'] = FALSE;
-			$opts['show_in_admin_bar'] = TRUE;
-			$opts['show_in_menu'] = TRUE;
-			$opts['show_in_nav_menu'] = TRUE;
-			 
-			$opts['labels']['add_new'] = esc_html__( "Add New {$single}", 'wisdom' );
-			$opts['labels']['add_new_item'] = esc_html__( "Add New {$single}", 'wisdom' );
-			$opts['labels']['all_items'] = esc_html__( $plural, 'wisdom' );
-			$opts['labels']['edit_item'] = esc_html__( "Edit {$single}" , 'wisdom' );
-			$opts['labels']['menu_name'] = esc_html__( $plural, 'wisdom' );
-			$opts['labels']['name'] = esc_html__( $plural, 'wisdom' );
-			$opts['labels']['name_admin_bar'] = esc_html__( $single, 'wisdom' );
-			$opts['labels']['new_item'] = esc_html__( "New {$single}", 'wisdom' );
-			$opts['labels']['not_found'] = esc_html__( "No {$plural} Found", 'wisdom' );
-			$opts['labels']['not_found_in_trash'] = esc_html__( "No {$plural} Found in Trash", 'wisdom' );
-			$opts['labels']['parent_item_colon'] = esc_html__( "Parent {$plural} :", 'wisdom' );
-			$opts['labels']['search_items'] = esc_html__( "Search {$plural}", 'wisdom' );
-			$opts['labels']['singular_name'] = esc_html__( $single, 'wisdom' );
-			$opts['labels']['view_item'] = esc_html__( "View {$single}", 'wisdom' );
-			register_post_type( strtolower( $cpt_name ), $opts );
-} 
+	$labels = array(
+		'name'               => _x( 'Custom Post Types', 'post type general name', 'your-plugin-textdomain' ),
+		'singular_name'      => _x( 'Custom Post Type', 'post type singular name', 'your-plugin-textdomain' ),
+		'menu_name'          => _x( 'Custom Post Types', 'admin menu', 'your-plugin-textdomain' ),
+		'name_admin_bar'     => _x( 'Custom Post Type', 'add new on admin bar', 'your-plugin-textdomain' ),
+		'add_new'            => _x( 'Add New', 'Custom Post Type', 'your-plugin-textdomain' ),
+		'add_new_item'       => __( 'Add New Custom Post Type', 'your-plugin-textdomain' ),
+		'new_item'           => __( 'New Custom Post Type', 'your-plugin-textdomain' ),
+		'edit_item'          => __( 'Edit Custom Post Type', 'your-plugin-textdomain' ),
+		'view_item'          => __( 'View Custom Post Type', 'your-plugin-textdomain' ),
+		'all_items'          => __( 'All Custom Post Types', 'your-plugin-textdomain' ),
+		'search_items'       => __( 'Search Custom Post Types', 'your-plugin-textdomain' ),
+		'parent_item_colon'  => __( 'Parent Custom Post Types:', 'your-plugin-textdomain' ),
+		'not_found'          => __( 'No Custom Post Types found.', 'your-plugin-textdomain' ),
+		'not_found_in_trash' => __( 'No Custom Post Types found in Trash.', 'your-plugin-textdomain' )
+	);
+
+	$args = array(
+		'labels'               => $labels, // An array of labels for this post type. 
+        'description'          => __( 'Description.', 'your-plugin-textdomain' ), // A short descriptive summary of what the post type is.
+		'public'               => true,    // Whether a post type is intended for use publicly either via the admin interface or by front-end users.
+		'publicly_queryable'   => true,    // Whether queries can be performed on the front end for the post type as part of parse_request().
+		'show_ui'              => true,    // Whether to generate and allow a UI for managing this post type in the admin.
+		'show_in_menu'         => true,    // Whether to show this post type in admin menu. 
+		'show_in_admin_bar'    => true,    // Whether to make this post available via adminn bar.
+		'query_var'            => true,
+		'rewrite'              => array( 'slug' => 'book' ),
+		'capability_type'      => 'post',  // The string to use to build the read, edit, and delete capabilities.
+		'has_archive'          => true,    // Whether there should be post type archives, or if a string, the archive slug to use.
+		'hierarchical'         => false,   // Whether the post type is hierarchical (e.g. page).
+		'menu_position'        => null,    // The position in the menu order the post type should appear. default is null, means at the bottom.
+		'exclude_from_search'  => false,   // Whether to exclude posts in this post type from fron-end search.
+		'supports'             => array( 'title', 'editor', 'author', 'thumbnail', 'excerpt', 'comments' ), // Core feature(s) the post type supports.
+		'can_export'           => true,    // Whether to allow this post type to be exported. 
+		'delete_with_user'     => null,    // Whether to delete posts of this type when deleting a user. If true, posts of this type belonging to the user will be moved to trash when then user is deleted. If false, posts of this type belonging to the user will *not* be trashed or deleted. 
+	);
+
+	register_post_type( 'book', $args ); // registers the post type.
+}
 
 }
